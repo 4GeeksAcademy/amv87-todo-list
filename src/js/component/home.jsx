da72680fe143895c8a1694a862984c9d71c13803
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-11-08 20:40:54 +0000

    starting to work on to-do function

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,23 @@
-import React from "react";
+import React, { useState, Component } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	
+	const [newInput, setNewInput ] = useState('');
+	const [newTodo, setNewTodo ] = useState([]);
+	function addNewTodo(){
+		setNewTodo(newTodo.concat(newInput))
+		setNewInput('')
+	}
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+				<h1 className="todos-title">todos</h1>
+				<input value={newInput} onChange={(e)=>setNewInput(e.target.value)} type="text" name="firstName" />
+				{newTodo.map( (element, index) =><p key={index}>{element}</p>)}
+				<button onClick={addNewTodo}></button>
+				<p>{newTodo.length} items left</p>
+		</>
+				
 	);
 };
 
