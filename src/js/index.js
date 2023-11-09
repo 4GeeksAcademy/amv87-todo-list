a17c2d913e76bf93e028225f84e450f5a538f6f9
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-11-09 16:49:20 +0000

    updated home.js

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Todos from "./component/todolist.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Todos />, document.querySelector("#app"));
