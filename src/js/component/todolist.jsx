66d4bb94d0950e3c51f3f0a30105ffb0c9196faf
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-11-10 21:27:03 +0000

    src/js/component/todolist.jsx completed

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -4,18 +4,44 @@ const Todos = () => {
 	
 	const [newInput, setNewInput ] = useState('');
 	const [newTodo, setNewTodo ] = useState([]);
-	function addNewTodo(){
+	const [item, setItem ] = useState('items');
+	const [notask, setNotask ] = useState('No tasks. Add a task.');
+	function addNewTodo() {
 		setNewTodo(newTodo.concat(newInput))
 		setNewInput('')
+		if (newTodo.length === 0) {
+			setItem('item')
+			setNotask ('')
+		} else {
+			setItem('items')
+			setNotask ('')
+		}
 	}
-	
+
+	const deleteTodo = (index) => {
+		const eraseTodo = newTodo.filter((_, i) => i !== index);
+		setNewTodo(eraseTodo);
+		if (newTodo.length === 2) {
+			setItem('item')
+		} else if (newTodo.length === 1) {
+			setNotask ('No tasks. Add a task.')
+		} else {
+			setItem('items')
+		}
+	};
+
 	return (
-		<div className="notepad">
-				<h1 className="todos-title">todos</h1>
-				<input className="input-notes" value={newInput} onKeyDown={(e) => (e.keyCode === 13 ? addNewTodo(e) : null)} onChange={(e)=>setNewInput(e.target.value)} placeholder="What needs to be done?" type="text" name="firstName" />
-				{newTodo.map( (element, index) =><p className="element" key={index}>{element}</p>)}
-				<p className="footer">{newTodo.length} items left</p>
-        </div>
+		<>
+			<div className="notepad">
+				<h1 className="todos-title">to-do's</h1>
+				<input className="input-notes" value={newInput} onKeyDown={(e) => (e.keyCode === 13 ? addNewTodo(e) : null)} onChange={(e)=>setNewInput(e.target.value)} placeholder="What needs to be done?" type="text" />
+				{notask}
+				{newTodo.map((element, index) =><p className="element" key={index}>{element} <button className="x-button" onClick={()=>deleteTodo(index)} >⨉</button></p>)}
+				<p className="footer">{newTodo.length} {item} left</p>
+       		</div>
+			<div className="notepad1"></div>
+			<div className="notepad2"></div>
+		</>
 	);
 };
 
